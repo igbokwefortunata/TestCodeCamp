1f7b3fddd681d0223b9acca0e82f3d3978381b5b
igbokwefortunata <igbokwefortunata.com>
2022-09-11 15:00:57 +0100

    Added java project, task 14, updated

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world."
+                +"\n my year"
++"\n I'm going to achieve my dreams in this software career."
+   +"\n This software program has not been easy."
+   +"\n I'm happy learning this program for a better tomorrow."
+    +"\n Consistency and determination is the key to success>"
+    +"\n Am going to do my best to become an expert software tester in future."
+    +"\n This is my 1st project on Java, hope to improve in future."
+     +"\n I wil keep pushing never to give up."
+     +"\n My target is high."
+     +"\n I learn from my mistakes>"
+     +"\n Happy new week.");
+
+    }
+
+
+
+
+
+}
+
